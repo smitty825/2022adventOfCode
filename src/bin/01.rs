31d7c71dfe9914027b67f7cffb2bfc75ae2d60ff
Dan Smith <smitty825@hotmail.com>
2022-12-01 20:52:44 -0800

    Got solution 2 working for day 1!

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -6,7 +6,6 @@ fn do_part_one(input: &str) -> Option<u32> {
     let mut current_elf = 0; 
     
     let lines = input.lines();
- //   println!("Size of lines {}", lines.len());
     for line in lines {
         let int_val = line.trim().parse::<u32>(); 
         if int_val.is_err() {
@@ -31,7 +30,30 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut all_elves : Vec<u32> = Vec::new(); 
+    let mut current_elf = 0; 
+    
+    let lines = input.lines();
+    for line in lines {
+        let int_val = line.trim().parse::<u32>(); 
+        if int_val.is_err() {
+            // end of elf
+            all_elves.push(current_elf); 
+            current_elf = 0;
+        }
+        else {
+            current_elf += int_val.unwrap();
+        }
+    } 
+
+    all_elves.sort(); 
+    let len = all_elves.len(); 
+    let sum = all_elves[len-1] + all_elves[len-2] + all_elves[len-3];
+    let optional_sum : Option<u32> = Some(sum); 
+
+    println!("{}", sum);
+
+    return optional_sum; 
 }
 
 fn main() {
